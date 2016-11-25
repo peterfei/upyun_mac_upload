b5ee57214e0ddf7864c50d09462fcbb18f0245d3
peterfei <peterfeispace@gmail.com>
2016-11-25 11:02:47 +0800

    init projects

--- Tuchuang/AppDelegate.swift
+++ Tuchuang/AppDelegate.swift
@@ -8,6 +8,9 @@
 
 import Cocoa
 
+var appDelegate: NSObject?
+var statusItem: NSStatusItem!
+
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
@@ -16,6 +19,14 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
     func applicationDidFinishLaunching(aNotification: NSNotification) {
         // Insert code here to initialize your application
+        appDelegate = self
+        statusItem = NSStatusBar.systemStatusBar().statusItemWithLength(NSSquareStatusItemLength)
+//        let statusBarButton = DragDestinationView(frame: (statusItem.button?.bounds)!)
+//        statusItem.button?.superview?.addSubview(statusBarButton, positioned: .Below, relativeTo: statusItem.button)
+        let iconImage = NSImage(named: "StatusIcon")
+        iconImage?.template = true
+        statusItem.button?.image = iconImage
+        statusItem.button?.target = self
     }
 
     func applicationWillTerminate(aNotification: NSNotification) {

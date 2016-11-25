f5e0ac040dcf4d6e69bda3a848081a0d8534bc3d
peterfei <peterfeispace@gmail.com>
2016-11-25 21:18:37 +0800

    复制图片显示在菜单栏

--- Tuchuang/AppDelegate.swift
+++ Tuchuang/AppDelegate.swift
@@ -12,13 +12,14 @@ import TMCache
 import Carbon
 var appDelegate: NSObject?
 var statusItem: NSStatusItem!
+var imagesCacheArr: [[String: AnyObject]] = Array()
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-
+    let pasteboardObserver = PasteboardObserver()
     @IBOutlet weak var window: NSWindow!
     @IBOutlet weak var statusMenu: NSMenu!
-    
+    @IBOutlet weak var uploadMenuItem: NSMenuItem!
     lazy var preferencesWindowController: NSWindowController = {
         
         let imageViewController = ImagePreferencesViewController()
@@ -30,6 +31,8 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     }()
     func applicationDidFinishLaunching(aNotification: NSNotification) {
         // Insert code here to initialize your application
+        registerHotKeys()
+        pasteboardObserver.addSubscriber(self)
         window.center()
         appDelegate = self
         statusItem = NSStatusBar.systemStatusBar().statusItemWithLength(NSSquareStatusItemLength)
@@ -46,6 +49,27 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         // Insert code here to tear down your application
     }
     func showMenu() {
+        let pboard = NSPasteboard.generalPasteboard()
+        let files: NSArray? = pboard.propertyListForType(NSFilenamesPboardType) as? NSArray
+        
+        if let files = files {
+            let i = NSImage(contentsOfFile: files.firstObject as! String)
+            i?.scalingImage()
+            uploadMenuItem.image = i
+            
+        } else {
+            let i = NSImage(pasteboard: pboard)
+            i?.scalingImage()
+            uploadMenuItem.image = i
+            
+        }
+        
+        let object = TMCache.sharedCache().objectForKey("imageCache")
+        if let obj = object as? [[String: AnyObject]] {
+            imagesCacheArr = obj
+            
+        }
+//        cacheImageMenuItem.submenu = makeCacheImageMenu(imagesCacheArr)
         statusItem.popUpStatusItemMenu(statusMenu)
     }
     @IBAction func statusMenuClicked(sender: NSMenuItem) {
@@ -66,7 +90,140 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         
         }
     }
+    
+    
+    func makeCacheImageMenu(imagesArr: [[String: AnyObject]]) -> NSMenu {
+        let menu = NSMenu()
+        if imagesArr.count == 0 {
+            let item = NSMenuItem(title: "没有历史", action: nil, keyEquivalent: "")
+            menu.addItem(item)
+        } else {
+            for index in 0..<imagesArr.count {
+                let item = NSMenuItem(title: "", action: #selector(cacheImageClick(_:)), keyEquivalent: "")
+                item.tag = index
+                let i = imagesArr[index]["image"] as? NSImage
+                i?.scalingImage()
+                item.image = i
+                menu.insertItem(item, atIndex: 0)
+            }
+        }
+        
+        return menu
+    }
+    
+    func cacheImageClick(sender: NSMenuItem) {
+        
+        NSPasteboard.generalPasteboard().clearContents()
+        
+        var picUrl = imagesCacheArr[sender.tag]["url"] as! String
+        
+        let fileName = NSString(string: picUrl).lastPathComponent
+        
+        if linkType == 0 {
+            picUrl = "![" + fileName + "](" + picUrl + ")"
+        }
+        
+        NSPasteboard.generalPasteboard().setString(picUrl, forType: NSStringPboardType)
+        NotificationMessage("图片链接获取成功", isSuccess: true)
+        
+    }
 
+    func NotificationMessage(message: String, informative: String? = nil, isSuccess: Bool = false) {
+        
+        let notification = NSUserNotification()
+        let notificationCenter = NSUserNotificationCenter.defaultUserNotificationCenter()
+        notificationCenter.delegate = appDelegate as? NSUserNotificationCenterDelegate
+        notification.title = message
+        notification.informativeText = informative
+        if isSuccess {
+            notification.contentImage = NSImage(named: "success")
+            notification.informativeText = "链接已经保存在剪贴板里，可以直接粘贴"
+        } else {
+            notification.contentImage = NSImage(named: "Failure")
+        }
+        
+        notification.soundName = NSUserNotificationDefaultSoundName;
+        notificationCenter.scheduleNotification(notification)
+        
+    }
     
 }
 
+extension AppDelegate: NSUserNotificationCenterDelegate, PasteboardObserverSubscriber {
+    // 强行通知
+    func userNotificationCenter(center: NSUserNotificationCenter, shouldPresentNotification notification: NSUserNotification) -> Bool {
+        return true
+    }
+    
+    override func observeValueForKeyPath(keyPath: String?, ofObject object: AnyObject?, change: [String: AnyObject]?, context: UnsafeMutablePointer<Void>) {
+        
+        print(change)
+        
+    }
+    
+    func pasteboardChanged(pasteboard: NSPasteboard) {
+//        QiniuUpload(pasteboard)
+        
+    }
+    
+    func registerHotKeys() {
+        
+        var gMyHotKeyRef: EventHotKeyRef = nil
+        var gMyHotKeyIDU = EventHotKeyID()
+        var gMyHotKeyIDM = EventHotKeyID()
+        var eventType = EventTypeSpec()
+        
+        eventType.eventClass = OSType(kEventClassKeyboard)
+        eventType.eventKind = OSType(kEventHotKeyPressed)
+        gMyHotKeyIDU.signature = OSType(32)
+        gMyHotKeyIDU.id = UInt32(kVK_ANSI_U);
+        gMyHotKeyIDM.signature = OSType(46);
+        gMyHotKeyIDM.id = UInt32(kVK_ANSI_M);
+        
+        RegisterEventHotKey(UInt32(kVK_ANSI_U), UInt32(cmdKey), gMyHotKeyIDU, GetApplicationEventTarget(), 0, &gMyHotKeyRef)
+        
+        RegisterEventHotKey(UInt32(kVK_ANSI_M), UInt32(controlKey), gMyHotKeyIDM, GetApplicationEventTarget(), 0, &gMyHotKeyRef)
+        
+        // Install handler.
+        InstallEventHandler(GetApplicationEventTarget(), { (nextHanlder, theEvent, userData) -> OSStatus in
+            var hkCom = EventHotKeyID()
+            GetEventParameter(theEvent, EventParamName(kEventParamDirectObject), EventParamType(typeEventHotKeyID), nil, sizeof(EventHotKeyID), nil, &hkCom)
+            switch hkCom.id {
+            case UInt32(kVK_ANSI_U):
+                let pboard = NSPasteboard.generalPasteboard()
+            //                QiniuUpload(pboard)
+            case UInt32(kVK_ANSI_M):
+                if linkType == 0 {
+                    linkType = 1
+                    NSNotificationCenter.defaultCenter().postNotificationName("MarkdownState", object: 1)
+                    guard let imagesCache = imagesCacheArr.last else {
+                        return 33
+                    }
+                    NSPasteboard.generalPasteboard().clearContents()
+                    let picUrl = imagesCache["url"] as! String
+                    NSPasteboard.generalPasteboard().setString(picUrl, forType: NSStringPboardType)
+                    
+                }
+                else {
+                    linkType = 0
+                    NSNotificationCenter.defaultCenter().postNotificationName("MarkdownState", object: 0)
+                    guard let imagesCache = imagesCacheArr.last else {
+                        return 33
+                    }
+                    NSPasteboard.generalPasteboard().clearContents()
+                    var picUrl = imagesCache["url"] as! String
+                    let fileName = NSString(string: picUrl).lastPathComponent
+                    picUrl = "![" + fileName + "](" + picUrl + ")"
+                    NSPasteboard.generalPasteboard().setString(picUrl, forType: NSStringPboardType)
+                }
+            default:
+                break
+            }
+            
+            return 33
+            /// Check that hkCom in indeed your hotkey ID and handle it.
+            }, 1, &eventType, nil, nil)
+        
+    }
+    
+}
---
+++ Tuchuang/AppHelper.swift
@@ -0,0 +1,81 @@
+//
+//  AppHelper.swift
+//  图床
+//
+//  Created by peterfei on 2016/11/25.
+//  Copyright © 2016年 peterfei. All rights reserved.
+//
+
+import Foundation
+import AppKit
+import TMCache
+
+extension NSImage {
+    func resizeImage(width: CGFloat, _ height: CGFloat) -> NSImage {
+        let img = NSImage(size: CGSizeMake(width, height))
+        img.lockFocus()
+        let ctx = NSGraphicsContext.currentContext()
+        ctx?.imageInterpolation = .High
+        self.drawInRect(NSMakeRect(0, 0, width, height), fromRect: NSMakeRect(0, 0, size.width, size.height), operation: .CompositeCopy, fraction: 1)
+        img.unlockFocus()
+        return img
+    }
+    
+    func scalingImage() {
+        let sW = self.size.width
+        let sH = self.size.height
+        let nW: CGFloat = 100
+        let nH = nW * sH / sW
+        self.size = CGSize(width: nW, height: nH)
+    }
+    
+}
+
+func adduploadImageToCache(dic: [String: AnyObject]) {
+    if imagesCacheArr.count < 5 {
+        imagesCacheArr.append(dic)
+        TMCache.sharedCache().setObject(imagesCacheArr, forKey: "imageCache")
+    } else {
+        imagesCacheArr.removeAtIndex(0)
+        imagesCacheArr.append(dic)
+        TMCache.sharedCache().setObject(imagesCacheArr, forKey: "imageCache")
+    }
+}
+
+private let pngHeader: [UInt8] = [0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A]
+private let jpgHeaderSOI: [UInt8] = [0xFF, 0xD8]
+private let jpgHeaderIF: [UInt8] = [0xFF]
+private let gifHeader: [UInt8] = [0x47, 0x49, 0x46]
+
+enum ImageFormat {
+    case Unknown, PNG, JPEG, GIF
+}
+
+extension NSData {
+    var kf_imageFormat: ImageFormat {
+        var buffer = [UInt8](count: 8, repeatedValue: 0)
+        self.getBytes(&buffer, length: 8)
+        if buffer == pngHeader {
+            return .PNG
+        } else if buffer[0] == jpgHeaderSOI[0] &&
+            buffer[1] == jpgHeaderSOI[1] &&
+            buffer[2] == jpgHeaderIF[0]
+        {
+            return .JPEG
+        } else if buffer[0] == gifHeader[0] &&
+            buffer[1] == gifHeader[1] &&
+            buffer[2] == gifHeader[2]
+        {
+            return .GIF
+        }
+        
+        return .Unknown
+    }
+}
+
+func getDateString() -> String {
+    let dateformatter = NSDateFormatter()
+    dateformatter.dateFormat = "YYYYMMdd"
+    let dataString = dateformatter.stringFromDate(NSDate(timeInterval: 0, sinceDate: NSDate()))
+    return dataString
+}

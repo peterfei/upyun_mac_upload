b2d3d0319b7eb5a47d72afb96084f879f657847a
peterfei <peterfeispace@gmail.com>
2016-11-25 09:59:01 +0800

    Initial Commit

---
+++ Tuchuang/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Tuchuang
+//
+//  Created by peterfei on 2016/11/25.
+//  Copyright © 2016年 peterfei. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
